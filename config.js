e2d43e7ddcf5414f1d7524c30fa8e246905a9f1a
florianmaxim <hello@florianmaxim>
2019-01-28 11:36:36 +0100

    yay

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
         toolbar: true
     },
     colors: {
-        primary: '#4a9fe0',
+        primary: ['#A6B3FF', '#6652FF', '#F022EB', '#EBEBFF'],
         gradient: 'linear-gradient(to bottom, #4a9fe0 0%,#e102ff 100%)'
     }
 }
