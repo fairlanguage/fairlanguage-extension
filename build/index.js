6b11d84252d735239bf80a89880cb374ef83fce9
florianmaxim <hello@florianmaxim.com>
2019-01-30 16:25:45 +0100

    Updated STATUS

--- build/index.js
+++ build/index.js
@@ -30130,7 +30130,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57110" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62394" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
