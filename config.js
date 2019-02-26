21ae86ed7427a8128d50ebef8f9095b19e061013
florianmaxim <hello@florianmaxim>
2019-02-26 20:05:58 +0100

    Added quick replacing feature

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ export default {
     dev: true,
   },
   colors: {
-    primary: ['#40B3FF', '#6652FF', '#F022EB', '#EBEBFF'],
+    primary: ['#40B3FF', '#6652FF', '#F022EB', '#d43aff'],
     gradient: 'linear-gradient(to bottom, #4a9fe0 0%,#e102ff 100%)',
   },
   toolbar: {
